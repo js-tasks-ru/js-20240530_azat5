df5c6414c640364b54909c395cb2527efa9abbe1
Azat Galikhanov <azat@galikhanov.com>
2024-06-21 00:06:21 +0200

    Solve 06-3

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -9,7 +9,6 @@ const Helpers = {
 const defaultFormatValue = (v) => v;
 
 export default class DoubleSlider {
-  currentDragElement;
   constructor(
     {
       min = 0,
@@ -20,85 +19,86 @@ export default class DoubleSlider {
   ) {
     this.min = min;
     this.max = max;
-    this.formatValue = formatValue;
     this.from = selected.from;
     this.to = selected.to;
+    this.formatValue = formatValue;
+    this.range = this.max - this.min;
     this.element = Helpers.createElementFromTemplate(this._createTemplate());
     this.sliderInner = this.element.querySelector(".range-slider__inner");
     this.thumbLeftElement = this.element.querySelector(".range-slider__thumb-left");
     this.thumbRightElement = this.element.querySelector(".range-slider__thumb-right");
     this.progressElement = this.element.querySelector(".range-slider__progress");
-    this.element.addEventListener("pointerdown", this._handlePointerDownOnLeftThumb);
-    this.element.addEventListener("pointerdown", this._handlePointerDownOnRightThumb);
+    this.fromElement = this.element.querySelector("[data-element=from]");
+    this.toElement = this.element.querySelector("[data-element=to]");
+    this.element.addEventListener("pointerdown", this._handlePointerDown);
   }
 
   destroy() {
-    this.element.removeEventListener("pointerdown", this._handlePointerDownOnLeftThumb);
-    this.element.removeEventListener("pointerdown", this._handlePointerDownOnRightThumb);
+    this.element.removeEventListener("pointerdown", this._handlePointerDown);
     this.element.remove();
   }
 
-  _handlePointerDownOnLeftThumb = (e) => {
-    const targetElement = e.target.closest(".range-slider__thumb-left");
-    if (!targetElement) { return; }
+  _handlePointerDown = (e) => {
+    switch (e.target) {
+    case this.thumbLeftElement:
+      this.draggable = "left";
+      break;
+    case this.thumbRightElement:
+      this.draggable = "right";
+      break;
+    default:
+      return;
+    }
 
-    document.addEventListener("pointermove", this._handlePointerMoveOnLeftThumb);
-    document.addEventListener("pointerup", this._handlePointerUpOnLeftThumb);
+    document.addEventListener("pointermove", this._handlePointerMove);
+    document.addEventListener("pointerup", this._handlePointerUp);
   }
 
-  _handlePointerMoveOnLeftThumb = (e) => {
+  _handlePointerMove = (e) => {
+    switch (this.draggable) {
+    case "left":
+      this.from =
+        [this.min, this._calculateSelectedValue(e), this.to]
+        .sort((a, b) => a - b)[1];
+      break;
+    case "right":
+      this.to =
+        [this.from, this._calculateSelectedValue(e), this.max]
+        .sort((a, b) => a - b)[1];
+      break;
+    default:
+      return;
+    }
+
+    this._updateStylesAndLabels();
+  }
+
+  _handlePointerUp = (_e) => {
+    this._dispatchEvent();
+    document.removeEventListener("pointerup", this._handlePointerUp);
+    document.removeEventListener("pointermove", this._handlePointerMove);
+  }
+
+  _calculateSelectedValue(moveEvent) {
     const containerWidth = this.sliderInner.getBoundingClientRect().width;
     const containerLeftAbs = this.sliderInner.offsetLeft;
-    const pointerLeftAbs = e.clientX;
+    const pointerLeftAbs = moveEvent.clientX;
     const pointerLeftRel = pointerLeftAbs - containerLeftAbs;
-    const selected = Math.round(pointerLeftRel / containerWidth * (this.max - this.min)) + this.min;
-
-    this.from = [this.min, selected, this.to].sort((a, b) => a - b)[1];
-    let leftPercent = (this.from - this.min) / (this.max - this.min) * 100;
-    this.thumbLeftElement.style.left = `${leftPercent}%`;
-
-    this._updateValues();
+    return Math.round(pointerLeftRel / containerWidth * this.range) + this.min;
   }
 
-  _handlePointerUpOnLeftThumb = (_e) => {
-    this._dispatchEvent();
-    document.removeEventListener("pointerup", this._handlePointerUpOnLeftThumb);
-    document.removeEventListener("pointermove", this._handlePointerMoveOnLeftThumb);
-  }
+  _updateStylesAndLabels() {
+    const leftPosition = `${(this.from - this.min) / this.range * 100}%`;
+    const rightPosition = `${(this.max - this.to) / this.range * 100}%`;
 
-  _handlePointerDownOnRightThumb = (e) => {
-    const targetElement = e.target.closest(".range-slider__thumb-right");
-    if (!targetElement) { return; }
+    this.thumbLeftElement.style.left = leftPosition;
+    this.thumbRightElement.style.right = rightPosition;
 
-    document.addEventListener("pointermove", this._handlePointerMoveOnRightThumb);
-    document.addEventListener("pointerup", this._handlePointerUpOnRightThumb);
-  }
+    this.progressElement.style.left = leftPosition;
+    this.progressElement.style.right = rightPosition;
 
-  _handlePointerMoveOnRightThumb = (e) => {
-    const containerWidth = this.sliderInner.getBoundingClientRect().width;
-    const containerLeftAbs = this.sliderInner.offsetLeft;
-    const pointerLeftAbs = e.clientX;
-    const pointerLeftRel = pointerLeftAbs - containerLeftAbs;
-    const selected = Math.round(pointerLeftRel / containerWidth * (this.max - this.min)) + this.min;
-
-    this.to = [this.max, selected, this.from].sort((a, b) => a - b)[1];
-    let rightPercent = (this.max - this.to) / (this.max - this.min) * 100;
-    this.thumbRightElement.style.right = `${rightPercent}%`;
-
-    this._updateValues();
-  }
-
-  _handlePointerUpOnRightThumb = (_e) => {
-    this._dispatchEvent();
-    document.removeEventListener("pointerup", this._handlePointerUpOnRightThumb);
-    document.removeEventListener("pointermove", this._handlePointerMoveOnRightThumb);
-  }
-
-  _updateValues() {
-    this.progressElement.style.left = `${(this.from - this.min) / (this.max - this.min) * 100}%`;
-    this.progressElement.style.right = `${(this.max - this.to) / (this.max - this.min) * 100}%`;
-    this.element.querySelector("[data-element=from]").textContent = this.formatValue(this.from);
-    this.element.querySelector("[data-element=to]").textContent = this.formatValue(this.to);
+    this.fromElement.textContent = this.formatValue(this.from);
+    this.toElement.textContent = this.formatValue(this.to);
   }
 
   _dispatchEvent() {
