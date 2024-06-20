a53b7f85928451019ea1567e825d6a881b608851
Azat Galikhanov <azat@galikhanov.com>
2024-06-20 09:39:09 +0200

    Solve 06-2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,61 @@
-class Tooltip {
-  initialize () {
+const Helpers = {
+  createElementFromTemplate(template) {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = template;
+    return tempDiv.firstElementChild;
+  }
+};
 
+export default class Tooltip {
+  static instance;
+  static showEventName = "pointerover";
+  static hideEventName = "pointerout";
+
+  constructor() {
+    if (Tooltip.instance) { return Tooltip.instance; }
+    Tooltip.instance = this;
+  }
+
+  initialize() {
+    this.element = Helpers.createElementFromTemplate(this._createTemplate());
+
+    document.body.addEventListener(Tooltip.showEventName, this._handleShow);
+    document.body.addEventListener(Tooltip.hideEventName, this._handleHide);
+  }
+
+  destroy() {
+    document.body.removeEventListener(Tooltip.showEventName, this._handleShow);
+    document.body.removeEventListener(Tooltip.hideEventName, this._handleHide);
+    this.element.remove();
+  }
+
+  render(message) {
+    this.element.textContent = message;
+    document.body.append(this.element);
+  }
+
+  _handleShow = (e) => {
+    const targetElement = e.target.closest("[data-tooltip]");
+    if (!targetElement) { return; }
+
+    const message = targetElement.dataset.tooltip;
+    this.element.textContent = message;
+
+    targetElement.append(this.element);
+  }
+
+  _handleHide = (e) => {
+    const targetElement = e.target.closest("[data-tooltip]");
+    if (!targetElement) { return; }
+
+    this.element.remove();
+  }
+
+  _createTemplate() {
+    return `
+      <div class="tooltip">
+        ${this.message}
+      </div>
+    `;
   }
 }
-
-export default Tooltip;
